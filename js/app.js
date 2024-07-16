5131374aef47f1fe532b05d18256f853d661133b
Cesar Yeguez <Cesar.Yeguez@gmail.com>
2024-07-16 12:40:48 -0400

    culminar estilos del menu mobile

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
-const botonMenu = document.querySelector('.header__icon-menu');
-const botonCerrar = document.querySelector('.header__icon-close');
-const nav = document.querySelector('.nav');
+const botonMenu = document.querySelector(".header__icon-menu");
+const botonCerrar = document.querySelector(".header__icon-close");
+const nav = document.querySelector(".nav");
 
-botonMenu.addEventListener('click',() => {
-  botonCerrar.style.display = 'inline-block';
-  botonMenu.style.display = 'none';
-  nav.style.display = 'flex';
-  
+botonMenu.addEventListener("click", () => {
+  botonCerrar.style.display = "inline-block";
+  botonMenu.style.display = "none";
+  nav.style.transform = "scale(1)";
 });
-botonCerrar.addEventListener('click',() => {
-  botonCerrar.style.display = 'none';
-  botonMenu.style.display = 'inline-block';
+botonCerrar.addEventListener("click", () => {
+  botonCerrar.style.display = "none";
+  botonMenu.style.display = "inline-block";
+  nav.style.transform = "scale(0)";
 });
