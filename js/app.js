5f6f6d14e8a3f6684d3c184b126bd3c0186ae5cd
Cesar Yeguez <Cesar.Yeguez@gmail.com>
2025-05-25 10:48:38 -0400

    Agregar ruta nueva de los estilos

--- js/app.js
+++ js/app.js
@@ -2,13 +2,17 @@ const botonMenu = document.querySelector(".header__icon-menu");
 const botonCerrar = document.querySelector(".header__icon-close");
 const nav = document.querySelector(".nav");
 
-botonMenu.addEventListener("click", () => {
-  botonCerrar.style.display = "inline-block";
-  botonMenu.style.display = "none";
-  nav.style.transform = "scale(1)";
-});
-botonCerrar.addEventListener("click", () => {
-  botonCerrar.style.display = "none";
-  botonMenu.style.display = "inline-block";
-  nav.style.transform = "scale(0)";
-});
+var screenWidth = window.innerWidth;
+
+if (screenWidth < 768) {
+  botonMenu.addEventListener("click", () => {
+    botonCerrar.style.display = "inline-block";
+    botonMenu.style.display = "none";
+    nav.style.transform = "scale(1)";
+  });
+  botonCerrar.addEventListener("click", () => {
+    botonCerrar.style.display = "none";
+    botonMenu.style.display = "inline-block";
+    nav.style.transform = "scale(0)";
+  });
+}
